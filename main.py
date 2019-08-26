4946213d85443fa7f998d06a8278341827dd28ec
Steven Rouk <stevenrouk@gmail.com>
2019-08-26 07:33:48 -0600

    Add primes func

--- main.py
+++ main.py
@@ -3,6 +3,15 @@
 def hello_world():
     print("Hello world!")
 
+def primes(n):
+    primes = []
+    for i in range(2, n+1):
+        for p in primes:
+            if i % p == 0:
+                primes.append(i)
+                break
+    return primes
+
 if __name__ == '__main__':
     hello_world()
 
