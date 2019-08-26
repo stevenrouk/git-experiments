469fe827730e9eb1f66858f43a8cbe52d8bd2063
Steven Rouk <stevenrouk@gmail.com>
2019-08-26 09:48:52 -0600

    Get first test working with module import.

--- tests/test.py
+++ tests/test.py
@@ -1,13 +1,11 @@
-# Importing main.py this way, per Real Python recommendation:
-# https://realpython.com/python-testing/
-main_py = __import__("../src/main.py")
-
 import unittest
+
+import src.experiment_funcs as experiment_funcs
 
 class TestHelloWorld(unittest.TestCase):
 
     def test_print(self):
-        self.assertEqual(main_py.hello_world(), "Hello world!")
+        self.assertEqual(experiment_funcs.hello_world(), "Hello world!")
 
 if __name__ == "__main__":
     unittest.main()
