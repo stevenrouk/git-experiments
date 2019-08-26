3c06179d9335c8cb35f527c90425e8b32f5119c7
Steven Rouk <stevenrouk@gmail.com>
2019-08-26 07:36:50 -0600

    Fix prime func

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ def hello_world():
 def primes(n):
     primes = []
     for i in range(2, n+1):
+        is_prime = True
         for p in primes:
             if i % p == 0:
-                primes.append(i)
+                is_prime = False
                 break
+        if is_prime:
+            primes.append(i)
     return primes
 
 if __name__ == '__main__':
     hello_world()
+    print(primes(11))
 
